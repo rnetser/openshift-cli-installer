9fa75b86c9656cbb12296040be1d8d7653984227
Meni Yakove <441263+myakove@users.noreply.github.com>
2023-10-10 16:00:45 +0300

    change home dir before run oc login (#235)
    * change home dir before run oc login
    
    * change home dir in the right place
    
    * Fix oc login after change HOME dir

--- openshift_cli_installer/utils/cli_utils.py
+++ openshift_cli_installer/utils/cli_utils.py
@@ -1,4 +1,5 @@
 import ast
+import contextlib
 import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
@@ -8,6 +9,7 @@ import rosa.cli
 from clouds.aws.aws_utils import set_and_verify_aws_credentials
 from ocm_python_wrapper.cluster import Cluster
 from ocp_resources.utils import TimeoutWatch
+from simple_logger.logger import get_logger
 
 from openshift_cli_installer.libs.managed_clusters.helpers import (
     prepare_managed_clusters_data,
@@ -51,6 +53,8 @@ from openshift_cli_installer.utils.const import (
 )
 from openshift_cli_installer.utils.gcp import get_gcp_regions
 from openshift_cli_installer.utils.general import tts
+
+LOGGER = get_logger(name=__name__)
 
 
 def get_clusters_by_type(clusters):
@@ -665,11 +669,12 @@ def assert_unique_cluster_names(clusters):
 
 def save_kubeadmin_token_to_clusters_install_data(clusters):
     # Do not run this function in parallel, get_kubeadmin_token() do `oc login`.
-    for cluster_data in clusters:
-        with get_kubeadmin_token(cluster_data=cluster_data) as kubeadmin_token:
-            cluster_data["kubeadmin-token"] = kubeadmin_token
+    with change_home_environment():
+        for cluster_data in clusters:
+            with get_kubeadmin_token(cluster_data=cluster_data) as kubeadmin_token:
+                cluster_data["kubeadmin-token"] = kubeadmin_token
 
-        dump_cluster_data_to_file(cluster_data=cluster_data)
+            dump_cluster_data_to_file(cluster_data=cluster_data)
 
     return clusters
 
@@ -692,3 +697,17 @@ def assert_boolean_values(clusters, create):
                     msg=f"The following keys must be booleans: {non_bool_keys}",
                 )
                 raise click.Abort()
+
+
+@contextlib.contextmanager
+def change_home_environment():
+    home_atr = "HOME"
+    tmp_home_dir = "/tmp/"
+    LOGGER.info(f"Changing {home_atr} environment variable to {tmp_home_dir}")
+    current_home = os.environ.get(home_atr)
+    os.environ[home_atr] = tmp_home_dir
+    yield
+    LOGGER.info(
+        f"Changing {home_atr} environment variable to previous value. {current_home}"
+    )
+    os.environ[home_atr] = current_home
--- openshift_cli_installer/utils/clusters.py
+++ openshift_cli_installer/utils/clusters.py
@@ -217,6 +217,10 @@ def get_kubeadmin_token(cluster_data):
         os.path.join(cluster_data["install-dir"], "auth", "kubeadmin-password")
     ) as fd:
         kubeadmin_password = fd.read()
-    run_command(shlex.split(f"oc login -u kubeadmin -p {kubeadmin_password}"))
+    run_command(
+        shlex.split(
+            f"oc login {cluster_data['api-url']} -u kubeadmin -p {kubeadmin_password}"
+        )
+    )
     yield run_command(shlex.split("oc whoami -t"))[1].strip()
     run_command(shlex.split("oc logout"))
